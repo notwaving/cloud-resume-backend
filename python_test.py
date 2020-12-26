fac380adcdc15ab68eb76ebb14480a1f7253f5ac
notwaving <samanthaburke.uk@gmail.com>
2020-12-26 23:47:46 +0000

    adds .gitignore, and another passing test

--- python_test.py
+++ python_test.py
@@ -2,7 +2,7 @@ import boto3
 import os
 import pytest
 from moto import mock_dynamodb2
-# from lambda.visitor_count import lambda_handler
+from VisitorCountFunction.visitor_count import lambda_handler
 
 
 def test_Env():
@@ -12,15 +12,14 @@ def test_Env():
     os.environ["AWS_DEFAULT_REGION"] = DEFAULT_REGION
 
 
-# @mock_dynamodb2
-# def test_negative():
-#     # os.environ["databaseName"] = 'ddbTableName'
+@mock_dynamodb2
+def test_negative():
 
-#     response = lambda_handler(0, 0)
-#     print("Response: ", response)
+    response = lambda_handler(0, 0)
+    print("Response: ", response)
 
-#     message = "Status code is not 200"
-#     assert 200 == response['statusCode'], message
+    message = "Status code is not 200"
+    assert 200 == response['statusCode'], message
 
 
 if __name__ == "__main__":
